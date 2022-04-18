a8b14db6978c678c7212eb1ca536bac0261954f9
Bentebent <henriksson.tim@gmail.com>
2022-04-18 22:18:40 +0200

    Basic window

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from time import perf_counter, sleep
+import pyglet
 
 from ECS.world import World
 
 from Components.test_components import TestComponentA, TestComponentB
 from Systems.test_system import TestSystem
 
-def test(*argv):
-    return TestComponentA(*argv)
+def initialze_window(width: int=1280, height: int=720, fullscreen=False, resizable=True, 
+                     caption="3D Programming Test") -> pyglet.window:
+    window = pyglet.window.Window(width=width, height=height, fullscreen=fullscreen, resizable=resizable, caption=caption)
+    return window
+    
     
 def initialize_world() -> World:
     world: World = World([TestComponentA, TestComponentB], [TestSystem])
@@ -21,8 +25,13 @@ def initialize_world() -> World:
     world.remove_entity(0)
 
     return world
+
+def on_render(window: pyglet.window):
+    window.clear()
+    window.flip()
     
 def main() -> None:
+    window = initialze_window()
     world: World = initialize_world()
     
     min_frame_rate: float = 30.0
@@ -34,11 +43,16 @@ def main() -> None:
     delta_time: float = min_frame_time
     
     while(True):
+        window.switch_to()
+        window.dispatch_events()
+        
         start_time = perf_counter()
         world.run(delta_time)
         end_time = perf_counter()
         
         delta_time = end_time - start_time
+        
+        on_render(window)
         
         if delta_time > max_frame_time:
             delta_time = max_frame_time
