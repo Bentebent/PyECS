2516e359b057b9e4e97766b9301fd75ca56273d2
Bentebent <henriksson.tim@gmail.com>
2022-04-18 21:18:32 +0200

    Add system handler

---
+++ ECS/system_handler.py
@@ -0,0 +1,21 @@
+from typing import List, Type
+
+from .system_types import Entity, Aspect
+from .base_system import BaseSystem
+from .entity_container import EntityContainer
+
+
+class SystemHandler():
+    _systems: List[BaseSystem] = []
+    
+    def __init__(self, world, systems: List[Type]) -> None:
+        for system in systems:
+            self._systems.append(system(world))
+        
+    def entity_changed(self, entity: Entity, old_aspect: Aspect, new_aspect: Aspect) -> None:
+        for system in self._systems:
+            system.entity_changed(entity, old_aspect, new_aspect)
+        
+    def run(self, delta_time: float) -> None:
+        for system in self._systems:
+            system.run(delta_time)
